7033b35e907229feb2c3e3388dafd2465c6370d5
James Cutler <jcutler@hubspot.com>
2019-08-10 13:24:55 -0400

    Updates

---
+++ precache-manifest.d68cd4db291544ce78121d087b1ba6ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e528a6af8c6a2d96bcad2e404d7ba6",
+    "url": "/clickify/index.html"
+  },
+  {
+    "revision": "ac950268f5ea2226814b",
+    "url": "/clickify/static/css/main.0a017891.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clickify/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "ac950268f5ea2226814b",
+    "url": "/clickify/static/js/main.a886d8dc.chunk.js"
+  },
+  {
+    "revision": "d9a7fe426fd2285df716",
+    "url": "/clickify/static/js/runtime~main.81085890.js"
+  }
+]);
